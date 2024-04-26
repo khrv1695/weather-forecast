13d9427f13493a66922951585dedc3f40bf68389
hemanth <khrv1695@gmail.com>
2024-04-26 23:45:57 +0530

    detail

---
+++ src/app/components/forecast-detail/forecast-detail.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forecast-detail.component.html',
+  styleUrl: './forecast-detail.component.sass'
+})
+export class ForecastDetailComponent {
+  @Input() minTemparature: any = 0.0;
+  @Input() maxTemparature: any = 0.0;
+  @Input() time: string = "";
+  @Input() description: string = "";
+  @Input() iconName: string = "";
+  imgbaseUrl = "https://openweathermap.org/img/wn/";
+}
--- src/app/components/tab-content/tab-content.component.ts
+++ src/app/components/tab-content/tab-content.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ForecastComponent } from '../forecast/forecast.component';
+import { ForecastDetailComponent } from '../forecast-detail/forecast-detail.component';
 
 @Component({
   selector: 'app-tab-content',
   standalone: true,
-  imports: [CommonModule, ForecastComponent],
+  imports: [CommonModule, ForecastComponent, ForecastDetailComponent],
   templateUrl: './tab-content.component.html',
   styleUrl: './tab-content.component.sass'
 })
